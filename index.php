077f4acb45259d7939a423f44e483d2c6b01ecd6
ray34-cyber <rancha.adzany1@gmail.com>
2022-06-21 01:30:34 +0800

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident cupiditate vero, quia quasi consequatur dignissimos minus enim, perferendis id quisquam voluptatibus nisi ex sed consectetur veritatis qui, similique facere. Soluta?</p>
+</body>
+</html>
